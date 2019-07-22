9597171e23fc7fbf4cdebaeed17bc17968f7728e
Artmen_V <viliutich.artem@gmail.com>
2019-07-22 12:40:44 +0700

    animate 3d

---
+++ src/toggle.js
@@ -0,0 +1,21 @@
+import React, { useState } from 'react'
+import { useSpring, animated } from 'react-spring'
+
+const Toggle = () => {
+    const [ isToggled, setToggled ] = useState(false)
+    const fade = useSpring({
+        //opacity: isToggled ? 1 : 0,
+        color: isToggled ? 'tomato' : 'green',
+       // fontSize: isToggled ? '1.5rem' : '10em',
+        transform: isToggled ? 'translate3d(0, 0, 0)' : 'translate3d(0, -50px, 0)',
+
+     })
+    return (
+        <div>
+            <animated.h1 style={fade}>Hello</animated.h1>
+            <button onClick={() => setToggled(!isToggled)} >Toggle</button>
+        </div>
+    )
+}
+
+export default Toggle
